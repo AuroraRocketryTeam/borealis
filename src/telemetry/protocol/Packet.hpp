62fc07b776e2c1b5fe3a9f49a617bf675056fc6a
Alessandro Monticelli <alessandr.monticell4@studio.unibo.it>
2025-02-13 15:28:53 +0100

    [TELEMETRY] Create Packet struct

--- src/telemetry/protocol/Packet.hpp
+++ src/telemetry/protocol/Packet.hpp
@@ -1,6 +1,18 @@
 #pragma once
 
+#ifndef MAX_PACKET_SIZE
+#define MAX_PACKET_SIZE 199
+#endif
+
+#ifndef MAX_PAYLOAD_SIZE // Max packet size (199 bytes) - header size (9 bytes) - crc size (2 bytes)
+#define MAX_PAYLOAD_SIZE 188
+#endif
+
 #include <cstdint>
+#include <cstddef> // Per offsetof
+
+#pragma pack(push, 1) // Evita padding nelle strutture
+
 /**
  * @brief The packet header.
  *
@@ -8,11 +20,11 @@
 struct PacketHeader
 {
     uint8_t packetNumber;
-    uint8_t packetSize;
+    uint8_t totalChunks;
     uint8_t chunkNumber;
-    uint8_t chunkSize;
-    uint16_t length;
-    uint32_t timestamp;
+    uint8_t chunkSize;   // In bytes, max 114
+    uint8_t payloadSize; // In bytes, max 114
+    uint32_t timestamp;  // Unix timestamp
 };
 
 /**
@@ -21,7 +33,7 @@ struct PacketHeader
  */
 struct PacketPayload
 {
-    uint8_t data[114]; // Max packet size (200  bytes) - header size (80 bytes) - crc size (16 bytes)
+    uint8_t data[MAX_PAYLOAD_SIZE];
 };
 
 /**
@@ -35,28 +47,11 @@ struct Packet
     uint16_t crc;
 };
 
+#pragma pack(pop) // Ripristina l'allineamento predefinito
 /**
- * @brief Calculate the CRC16 of a data buffer.
+ * @brief Calculate the CRC16 of a packet.
  *
- * @param data the data buffer
- * @param length the length of the data buffer
- * @return uint16_t the CRC16 of the data buffer
+ * @param packet the packet (passed by reference)
+ * @return uint16_t the CRC16 of the packet
  */
-uint16_t calculateCRC(const uint8_t *data, size_t length)
-{
-    uint16_t crc = 0xFFFF;
-    while (length--)
-    {
-        crc ^= *data++;
-        for (uint8_t i = 0; i < 8; i++)
-        {
-            if (crc & 0x0001)
-                crc = (crc >> 1) ^ 0xA001;
-            else
-                crc = crc >> 1;
-        }
-    }
-    return crc;
-}
-
-#pragma pack(pop)
+uint16_t calculateCRC(const Packet &packet);
